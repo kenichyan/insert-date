98502de805da4f189f065fa22c66bb3c8e499868
Ken-ichi Yasuda <msredcomet06@gmail.com>
2025-05-14 18:25:19 +0900

    Not needed :norm kJ command is removed.
    Not Needed :norm kJ command is removed from the plugin.

--- lua/insert-date/init.lua
+++ lua/insert-date/init.lua
@@ -4,7 +4,6 @@ function M.setup()
   vim.api.nvim_create_user_command("Date", function()
     local datetime = os.date("%Y-%m-%d %I:%M:%S %p")
     vim.api.nvim_put({ datetime }, "c", false, true)
-    vim.cmd("normal! kJ")
   end, {})
 end
 
