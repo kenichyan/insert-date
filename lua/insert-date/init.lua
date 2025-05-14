40278769888fa5ef19bbd242cd35e6e59654c3f3
Ken-ichi Yasuda <msredcomet06@gmail.com>
2025-05-14 18:30:15 +0900

    Fixing the position of Date insertion.
    Fixing the position of insertion of the date.

--- lua/insert-date/init.lua
+++ lua/insert-date/init.lua
@@ -3,9 +3,18 @@ local M = {}
 function M.setup()
   vim.api.nvim_create_user_command("Date", function()
     local datetime = os.date("%Y-%m-%d %I:%M:%S %p")
-    vim.api.nvim_put({ datetime }, "c", false, true)
+
+    -- Get the current cursor position: (row, col)
+    local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+
+    -- Convert row to 0-based index (required by nvim_buf_set_text)
+    row = row - 1
+
+    -- Insert the datetime string at the exact cursor position
+    vim.api.nvim_buf_set_text(0, row, col, row, col, { datetime })
   end, {})
 end
 
 return M
+
 
