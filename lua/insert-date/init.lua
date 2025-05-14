a06d92aa6a6c989b1e86eec9b2ca7f19171f0091
Ken-ichi Yasuda <msredcomet06@gmail.com>
2025-05-14 18:20:47 +0900

    Insert position is fixed to the cursor position.
    Insert position is fixed from the next line to the current
    cursor position.

--- lua/insert-date/init.lua
+++ lua/insert-date/init.lua
@@ -3,7 +3,7 @@ local M = {}
 function M.setup()
   vim.api.nvim_create_user_command("Date", function()
     local datetime = os.date("%Y-%m-%d %I:%M:%S %p")
-    vim.api.nvim_put({ datetime }, "l", true, true)
+    vim.api.nvim_put({ datetime }, "c", false, true)
     vim.cmd("normal! kJ")
   end, {})
 end
